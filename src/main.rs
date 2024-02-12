1e5655c3a2bdb6a0ff8d821d53c2412bce960767
Tonie Victor <victornwanochi@gmail.com>
2024-02-13 00:22:59 +0100

    refcator: updated the parse_input function

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use std::env::args;
-use std::env::Args;
 use std::path::PathBuf;
 use std::process::exit;
 
@@ -9,51 +8,37 @@ struct Input {
 }
 
 fn main() {
-    let args = args();
+    let args: Vec<String> = args().collect();
     let input = parse_input(args);
 
     println!("{:?}", input.file_path);
     println!("{:?}", input.pattern);
 }
 
-fn parse_input(mut args: Args) -> Input {
+fn parse_input(args: Vec<String>) -> Input {
     match args.len() {
         3 => {
-            let pattern: String = match args.nth(1) {
-                Some(pat) => pat,
-                None => {
-                    println!("missing pattern");
-                    exit(1);
-                }
-            };
+            let pattern = &args[1];
+            let file_path = &args[2];
 
-            let file_path: String = match args.next() {
-                Some(fp) => fp,
-                None => {
-                    println!("no file provided");
-                    exit(1);
-                }
-            };
             let input = Input {
-                pattern,
+                pattern: String::from(pattern),
                 file_path: PathBuf::from(file_path),
             };
             input
         }
         2 => {
-            if let Some(arg) = args.nth(1) {
-                if arg == "--help" {
-                    println!("Usage ./zeteo <pattern> <file>");
-                    println!("pattern => string to look for");
-                    println!("file_path => path to the file to look through");
-                    exit(0);
-                }
+            if args[1] == "--help" {
+                println!("Usage ./zeteo <pattern> <file>\n");
+                println!("pattern => string to look for\n");
+                println!("file_path => path to the file to look through");
+                exit(0);
             }
-            println!("Usage: zeteo <pattern> <filepath>");
+            println!("Usage: {:?} <pattern> <filepath>", &args[0]);
             exit(1);
         }
         _ => {
-            println!("Usage: zeteo <pattern> <filepath>");
+            println!("Usage: {:?} <pattern> <filepath>", &args[0]);
             exit(1);
         }
     }
